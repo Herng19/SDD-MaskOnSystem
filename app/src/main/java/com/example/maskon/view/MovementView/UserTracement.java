c80afb05e30f8bd17f8f99beba17021011beeac9
atiqahkamal <nuratiqah1306@gmail.com>
2022-05-26 19:06:05 +0800

    Create Movement page

--- app/src/main/java/com/example/maskon/view/MovementView/UserTracement.java
+++ app/src/main/java/com/example/maskon/view/MovementView/UserTracement.java
@@ -1,21 +1,16 @@
 package com.example.maskon.view.MovementView;
 
-<<<<<<< Updated upstream
-public class CheckinMenu {
-}
-=======
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 import com.example.maskon.R;
 
-public class CheckInMenu extends AppCompatActivity {
+public class UserTracement extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_checkin_menu);
+        setContentView(R.layout.activity_user_tracement);
     }
 }
->>>>>>> Stashed changes
