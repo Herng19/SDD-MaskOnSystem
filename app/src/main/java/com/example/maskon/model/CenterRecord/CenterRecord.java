41799022bbc52bd3f94c07ec66b1547d79ec4f7d
Loo Chang Herng <loochangherng@gmail.com>
2022-06-01 01:53:37 +0800

    add image input

--- app/src/main/java/com/example/maskon/model/CenterRecord/CenterRecord.java
+++ app/src/main/java/com/example/maskon/model/CenterRecord/CenterRecord.java
@@ -6,26 +6,5 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-public class CenterRecord extends SQLiteOpenHelper {
-    public static final String DBNAME="maskOn.db";
-    public static final String TABLENAME="centerRecord";
-    public static final int VER=1;
-    String query;
-
-    public CenterRecord(@Nullable Context context) {
-        super(context, DBNAME, null, VER);
-    }
-
-    @Override
-    public void onCreate(SQLiteDatabase db) {
-        query="create table "+ TABLENAME + "(Center_ID integer primary key, Center_Name text, Address text, PIC text, Contact_Num text, Email text, Max_Capacity integer, Curr_Capacity integer)";
-        db.execSQL(query);
-    }
-
-    @Override
-    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
-        query="drop table if exists " + TABLENAME + "";
-        db.execSQL(query);
-        onCreate(db);
-    }
+public class CenterRecord{
 }
---
+++ app/src/main/java/com/example/maskon/model/DBMain.java
@@ -0,0 +1,31 @@
+package com.example.maskon.model;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBMain extends SQLiteOpenHelper {
+    public static final String DBNAME="maskOn.db";
+    public static final String TABLENAME="centerRecord";
+    public static final int VER=1;
+    String query;
+
+    public DBMain(@Nullable Context context) {
+        super(context, DBNAME, null, VER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        query="create table "+ TABLENAME + "(Center_ID integer primary key, Center_Image blob, Center_Name text, Address text, PIC text, Contact_Num text, Email text, Max_Capacity integer, Curr_Capacity integer)";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int i, int i1) {
+        query="drop table if exists " + TABLENAME + "";
+        db.execSQL(query);
+        onCreate(db);
+    }
+}
