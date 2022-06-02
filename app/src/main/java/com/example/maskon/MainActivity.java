2ed15e8b2542c180a7ddab7b01f154b648fc581d
Misha2799 <82268353+Misha2799@users.noreply.github.com>
2022-06-02 17:53:20 +0800

    correction of r import

--- app/src/main/java/com/example/maskon/MainActivity.java
+++ app/src/main/java/com/example/maskon/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.maskon;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.maskon.view.QuarantineRecordView.Homepg_Quarantine;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -32,6 +36,18 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
+
+        //Object button
+        //Intent intention
+        Button btnQuarantine = findViewById(R.id.button2);
+        btnQuarantine.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Homepg_Quarantine.class);
+                startActivity(intent);
+            }
+        });
     }
+
 
 }
--- app/src/main/java/com/example/maskon/view/CenterView/QuarantineCenterList.java
+++ app/src/main/java/com/example/maskon/view/CenterView/QuarantineCenterList.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.example.maskon.R;
+
 public class QuarantineCenterList extends AppCompatActivity {
 
     @Override
