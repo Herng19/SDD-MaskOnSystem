d0c748aebb7fe600a7f1598eacd855e1567771be
nraineyaa <98253793+nraineyaa@users.noreply.github.com>
2022-06-12 02:32:37 +0800

    Vaccine
    test

---
+++ app/src/main/java/com/example/maskon/view/RegistrationView/forgot_pass.java
@@ -0,0 +1,16 @@
+package com.example.maskon.view.RegistrationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.maskon.R;
+
+public class forgot_pass extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_pass);
+    }
+}
---
+++ app/src/main/java/com/example/maskon/view/RegistrationView/login_page.java
@@ -0,0 +1,16 @@
+package com.example.maskon.view.RegistrationView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.maskon.R;
+
+public class login_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page);
+    }
+}
