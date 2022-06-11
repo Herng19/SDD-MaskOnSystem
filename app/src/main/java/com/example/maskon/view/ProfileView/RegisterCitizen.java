5be1a53337e54c62df416f61ac78a2b2bc463099
FARRA ALIA <farraalia280@gmail.com>
2022-06-12 03:20:04 +0800

    Add RegisterCitizen

---
+++ app/src/main/java/com/example/maskon/view/ProfileView/RegisterCitizen.java
@@ -0,0 +1,16 @@
+package com.example.maskon.view.ProfileView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.maskon.R;
+
+public class RegisterCitizen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_citizen);
+    }
+}
