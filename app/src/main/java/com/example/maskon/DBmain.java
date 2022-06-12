864d475bc9fc57330e9e052bb37b891908af9eff
FARRA ALIA <farraalia280@gmail.com>
2022-06-12 11:22:59 +0800

    Update Login Interface

---
+++ app/src/main/java/com/example/maskon/DBmain.java
@@ -0,0 +1,28 @@
+package com.example.maskon;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class DBmain extends SQLiteOpenHelper {
+    public static final String DBNAME="profile.db";
+    public static final String TABLENAME="profile";
+    public static final int VER=1;
+    public DBmain(@Nullable Context context) {
+        super(context, DBNAME, null, VER);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query="create table"+TABLENAME+"(id integer primary key, avatar blob, name text)";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        String query="drop table if exists"+TABLENAME+"";
+        db.execSQL(query);
+    }
+}
