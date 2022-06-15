188899f52b0e8ea2f1b07310ed3d4ace4ecb49e0
FARRA ALIA <farraalia280@gmail.com>
2022-06-15 23:51:32 +0800

    Fix ui error

--- app/src/main/java/com/example/maskon/view/ProfileView/login.java
+++ app/src/main/java/com/example/maskon/view/ProfileView/login.java
@@ -2,7 +2,10 @@ package com.example.maskon.view.ProfileView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.maskon.R;
 
@@ -12,5 +15,15 @@ public class login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+        //move to profile visitor
+        Button btnOpenProfileVisitor = (Button) findViewById(R.id.login_btn);
+        btnOpenProfileVisitor.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(login.this, ProfileVistitor.class));
+            }
+        });
     }
 }
